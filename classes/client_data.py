460ee7904ae7853d528bc4b02394d77a860015d3
marionumza <marionumza@gmail.com>
2018-03-26 22:51:27 -0300

    Update client_data.py

--- classes/client_data.py
+++ classes/client_data.py
@@ -127,7 +127,7 @@ _clients = [
      'images': [
          {'name': 'aeroo', 'usr': 'jobiols', 'img': 'aeroo-docs'},
          {'name': 'odoo', 'usr': 'jobiols', 'img': 'odoo-jeo', 'ver': '8.0'},
-         {'name': 'postgres', 'usr': 'postgres', 'ver': '9.6'},
+         {'name': 'postgres', 'usr': 'postgres', 'ver': '9.5'},
          {'name': 'backup', 'usr': 'jobiols', 'img': 'backup'},
      ]
      },
