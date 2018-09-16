996faaf9cfcaf66a5057bf7d2fe51dd31cf6d54f
marionumza <marionumza@gmail.com>
2018-09-15 22:26:43 -0300

    m

--- classes/client_data.py
+++ classes/client_data.py
@@ -753,7 +753,7 @@ _clients = [
          {'usr': 'marionumza', 'repo': 'delivery-carrier', 'branch': '9.0'},
          {'usr': 'marionumza', 'repo': 'pos', 'branch': '9.0'},
          {'usr': 'OCA', 'repo': 'report-print-send', 'branch': '9.0'},
-         {'usr': 'odoo', 'repo': 'odoo-extra', 'branch': '9.0'},
+         #{'usr': 'odoo', 'repo': 'odoo-extra', 'branch': '9.0'},
          {'usr': 'JayVora-SerpentCS', 'repo': 'SerpentCS_Contributions', 'branch': '9.0'},
 
 
